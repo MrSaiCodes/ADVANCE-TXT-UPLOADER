80a8f85b6ff581f8c9d7bb5757f8e2459a872b82
MrSaiCodes <saiwebsite4us@gmail.com>
2025-04-17 09:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
 
-PORT = int(os.environ.get("PORT", 5000))
-
-app.run(host="0.0.0.0", port=PORT)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
