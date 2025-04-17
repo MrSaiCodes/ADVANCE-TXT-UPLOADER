242c894d6bbeccca6d59dc37790b31409099de32
MrSaiCodes <saiwebsite4us@gmail.com>
2025-04-17 09:43:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route('/')
-def hello_world():
-    return 'Tushar'
+PORT = int(os.environ.get("PORT", 5000))
 
-
-if __name__ == "__main__":
-    app.run()
+app.run(host="0.0.0.0", port=PORT)
